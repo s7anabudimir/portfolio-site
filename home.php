5cfd703385ca4d3d940f6031921413751bfa155e
s7anabudimir <ana.budimir@s7codedesign.com>
2020-08-12 15:30:46 +0200

    Fetched categories from blog posts.

--- home.php
+++ home.php
@@ -2,7 +2,28 @@
 <section id="content">
     <div class="content-wrap">
         <div class="container">
+        <?php
+    $args = array(                 
+		'cat' => '1',
+		'hide_empty' => true
+	); 
+    $categorylist = get_terms( $args ); 
+    ?>
+    <div class="container">
+    <div class="categories">
+        <ul>
+            <li class="js-filter">Show All
+            </li>
+            
+    <?php foreach($categorylist as $ctg) :
+    ?>
+            <li class="js-filter"><a href="<?php get_category_link($ctg->term_id); ?>"><?php echo $ctg->name; ?></a></li>
+    <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
             <div id="posts" class="small-thumbs">
+                
                 <?php get_template_part('includes/section', 'archive'); ?>
 
                 <?php previous_posts_link(); ?>
