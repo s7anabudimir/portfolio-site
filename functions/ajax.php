d92755464ebc8f8b6ab849554dad7bcddfeb3b97
s7anabudimir <ana.budimir@s7codedesign.com>
2020-08-13 16:05:42 +0200

    Search form created on the home page.

--- functions/ajax.php
+++ functions/ajax.php
@@ -45,4 +45,51 @@ die();
 }
 add_action('wp_ajax_filter', 'filter_ajax');
 add_action('wp_ajax_nopriv_filter', 'filter_ajax');
-?>
+
+
+
+//Search Form
+// the ajax function
+add_action('wp_ajax_data_fetch' , 'data_fetch');
+add_action('wp_ajax_nopriv_data_fetch','data_fetch');
+function data_fetch(){
+
+    $the_query = new WP_Query( array( 'posts_per_page' => 5, 'post_type' => 'post' ) );
+    if( $the_query->have_posts() ) :
+        while( $the_query->have_posts() ): $the_query->the_post(); ?>
+
+            <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+
+        <?php endwhile;
+        wp_reset_postdata();  
+    else: 
+        echo '<h3>No Results Found</h3>';
+    endif;
+        die();
+    }
+
+    // add the ajax fetch js
+    add_action( 'wp_footer', 'ajax_fetch' );
+    function ajax_fetch() {
+    ?>
+    <script type="text/javascript">
+    function fetchResults(){
+        var keyword = jQuery('#searchInput').val();
+        if(keyword == ""){
+            jQuery('#datafetch').html("");
+        } else {
+            jQuery.ajax({
+                url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                type: 'post',
+                data: { action: 'data_fetch', keyword: keyword  },
+                success: function(data) {
+                    jQuery('#posts').html( data );
+                }
+            });
+        }
+
+    }
+    </script>
+
+    <?php
+    }
