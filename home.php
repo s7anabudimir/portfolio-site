6d8049c7d34368e65377f5a27de7e2a44f447e4c
s7anabudimir <ana.budimir@s7codedesign.com>
2020-08-13 16:06:14 +0200

    Added functionality of the search page.

--- home.php
+++ home.php
@@ -10,14 +10,24 @@
             
         ?>
     <div class="container">
-    <div class="categories">
-        <?php foreach ( $categorylist as $ctg) : ?>
+        <div class="row">
+            <div class="col-lg-6">
+                <form action="" id="searchForm">
+                    <input type="text" name="s" placeholder="Type something here" class="form-control" id="searchInput" onkeyup="fetchResults()" value="<?php echo get_search_query(); ?>">
+                </form>
+            </div>
+        
+        <div class="col-lg-6">
+            <div class="categories">
+            <?php foreach ( $categorylist as $ctg) : ?>
             <!-- <li data-categories="<?php echo $ctg->term_id; ?>"><a href="<?php echo get_category_link( $ctg->term_id ); ?>"><?php echo $ctg->name; ?></a></li><br /> -->
             <li class="category-btn" data-categories="<?php echo $ctg->term_id; ?>"><a href="javascript:void(0);"><?php echo $ctg->name; ?></a></li><br />
-        <?php endforeach ?>
-    </div>
-</div>
-            <div id="posts" class="small-thumbs">
+            <?php endforeach ?>
+            </div>
+        </div>
+        </div>
+        </div>
+            <div id="posts" class="small-thumbs"> 
                 
                 <?php get_template_part('includes/section', 'archive'); ?>
 
@@ -50,8 +60,10 @@
                     success: function(result) {
                         $('#posts').html(result);
                     }
-                }) ;
-            })
+                });
+            }); 
+            
         })
+
     })(jQuery); 
 </script>
